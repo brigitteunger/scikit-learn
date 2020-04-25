2881a88e8d2975ef17bd7140fa7c485305c95588
Hannah <32333241+hhnnhh@users.noreply.github.com>
2020-04-25 22:17:50 +0200

    documentation issues: label changed and added for version 0.18

--- sklearn/feature_selection/_univariate_selection.py
+++ sklearn/feature_selection/_univariate_selection.py
@@ -383,7 +383,7 @@ class SelectPercentile(_BaseFilter):
         Default is f_classif (see below "See also"). The default function only
         works with classification tasks.
 
-        ..versionadded:: 0.18
+        .. versionadded:: 0.18
 
     percentile : int, optional, default=10
         Percent of features to keep.
@@ -468,7 +468,7 @@ class SelectKBest(_BaseFilter):
         Default is f_classif (see below "See also"). The default function only
         works with classification tasks.
 
-        ..versionadded:: 0.18
+        .. versionadded:: 0.18
 
     k : int or "all", optional, default=10
         Number of top features to select.
